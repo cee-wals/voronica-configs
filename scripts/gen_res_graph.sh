84404b6240621924b813b8219ef9ab242263f199
Chris Wilder <wilder.christopher@gmail.com>
2023-06-09 14:15:42 +0000

    Autocommit from 2023-06-09 14:15:42
    Klipper on commit: b17ae55
    
    Moonraker on commit: 6c7dfe5
    
    Mainsail version: v2.5.1
    
     version:

--- scripts/gen_res_graph.sh
+++ scripts/gen_res_graph.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 timestamp="$(date +%Y-%m-%d__%T)"
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /tmp/shaper_calibrate_x.png && cp /tmp/shaper_calibrate_x.png ~/printer_data/input-shaper/shaper_calibrate_x_${timestamp}.png
-~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /tmp/shaper_calibrate_y.png && cp /tmp/shaper_calibrate_y.png ~/printer_data/input-shaper/shaper_calibrate_x_${timestamp}.png
-cp /tmp/resonances_x_*.csv /tmp/resonances_y_*.csv ~/printer_data/input-shaper/
+#~/klipper/scripts/calibrate_shaper.py /tmp/resonances_x_*.csv -o /tmp/shaper_calibrate_x.png && cp /tmp/shaper_calibrate_x.png ~/printer_data/input-shaper/shaper_calibrate_x_${timestamp}.png
+~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_x_*.csv -o /tmp/shaper_calibrate_x.png && cp /tmp/shaper_calibrate_x.png ~/printer_data/input-shaper/shaper_calibrate_x_${timestamp}.png
+#~/klipper/scripts/calibrate_shaper.py /tmp/resonances_y_*.csv -o /tmp/shaper_calibrate_y.png && cp /tmp/shaper_calibrate_y.png ~/printer_data/input-shaper/shaper_calibrate_y_${timestamp}.png
+~/klipper/scripts/calibrate_shaper.py /tmp/calibration_data_y_*.csv -o /tmp/shaper_calibrate_y.png && cp /tmp/shaper_calibrate_y.png ~/printer_data/input-shaper/shaper_calibrate_y_${timestamp}.png
+#cp /tmp/resonances_x_*.csv /tmp/resonances_y_*.csv ~/printer_data/input-shaper/
+cp /tmp/calibration_data_x_*.csv /tmp/calibration_data_y_*.csv ~/printer_data/input-shaper/
